e342b0f631569edb286d73d4796b1178da04f16c
Oleg Pestov <ijk@dindilen.com>
2020-10-12 08:48:37 +0300

    1646
    Total time: 0.24s
    Input 4*10^5: 0.19s
    Prefix sums: 0.01s
    Output 2*10^5: 0.05
    
    sys.stdout.write was a little bit faster than print

---
+++ 1646_range_sum_queries_i/1646.py
@@ -0,0 +1,14 @@
+import sys
+
+n, q = map(int, sys.stdin.readline().split())
+aux = [int(x) for x in sys.stdin.read().split()]
+
+p = [0] * (n + 1)
+for i in range(1, n + 1):
+    p[i] = p[i - 1] + aux[i - 1]
+
+r = []
+for i in range(n, len(aux), 2):
+    a, b = aux[i], aux[i + 1]
+    r.append(str(p[b] - p[a - 1]))
+sys.stdout.write('\n'.join(r))
