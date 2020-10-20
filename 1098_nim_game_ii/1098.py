5fbdff4b2d38221cae50eb04c84ede5ecdc5a212
Oleg Pestov <ijk@dindilen.com>
2020-10-20 12:39:30 +0300

    1098

---
+++ 1098_nim_game_ii/1098.py
@@ -0,0 +1,15 @@
+import sys
+
+t = int(sys.stdin.readline())
+a = [int(x) for x in sys.stdin.read().split()]
+
+r = []
+i = 0
+for _ in range(t):
+    x = 0
+    for j in range(a[i]):
+        x ^= (a[i + j + 1] % 4)
+    r.append(str('first' if x else 'second'))
+    i += a[i] + 1
+
+sys.stdout.write('\n'.join(r))
