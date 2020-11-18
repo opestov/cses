f0b5f76465eeacc2f68e0253fb5180be27d2c787
Oleg Pestov <ijk@dindilen.com>
2020-11-18 10:57:16 +0300

    1682
    Simplify solution. It is enough to have two dfs'es.

--- 1682_flight_routes_check/1682_2.py
+++ 1682_flight_routes_check/1682_2.py
@@ -1,46 +1,52 @@
 def read():
     global n, m, d1, h1, d2, h2
     n, m = map(int, sys.stdin.readline().split())
-    a = [int(x) - 1 for x in sys.stdin.read().split()]
+    a = [int(x) for x in sys.stdin.read().split()]
     d1, h1, d2, h2 = [0] * m, [0] * (n + 1), [0] * m, [0] * (n + 1)
     for i in range(0, len(a), 2):
-        h1[a[i]] += 1
-        h2[a[i + 1]] += 1
+        h1[a[i] - 1] += 1
+        h2[a[i + 1] - 1] += 1
     for i in range(1, n + 1):
         h1[i] += h1[i - 1]
         h2[i] += h2[i - 1]
     for i in range(0, len(a), 2):
-        u, v = a[i], a[i + 1]
+        u, v = a[i] - 1, a[i + 1] - 1
         d1[h1[u] - 1] = v
         h1[u] -= 1
         d2[h2[v] - 1] = u
         h2[v] -= 1
 
 
-def check(d, h):
-    c = [False] * n
-    s = [0]
-    c[0] = True
+def dfs(start, d, h):
+    c[start] = True
+    s.append(start)
     while len(s) > 0:
         v = s.pop()
         for i in range(h[v], h[v + 1]):
             u = d[i]
             if not c[u]:
-                s.append(u)
                 c[u] = True
-    for i in range(n):
-        if not c[i]: return i
+                s.append(u)
 
 
 import sys
 
 read()
-x = check(d1, h1)
-if x != None:
-    sys.stdout.write(f'NO\n1 {x + 1}')
-    sys.exit(0)
-x = check(d2, h2)
-if x != None:
-    sys.stdout.write(f'NO\n{x + 1} 1')
-    sys.exit(0)
-sys.stdout.write('YES')
+
+c, s = [False] * n, []
+x = 0
+for i in range(n):
+    if not c[i]:
+        x = i
+        dfs(i, d1, h1)
+
+for i in range(n):
+    c[i] = False
+dfs(x, d2, h2)
+
+for i in range(n):
+    if not c[i]:
+        print('NO', file=sys.stdout)
+        print(i + 1, x + 1, file=sys.stdout)
+        sys.exit(0)
+print('YES', file=sys.stdout)
